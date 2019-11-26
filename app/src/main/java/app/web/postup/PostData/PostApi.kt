7817b801a939c55da3890ab9db801ca8637e5785
rlarlvy153 <rlarlvy153@naver.com>
2019-11-26 20:06:09 +0900

    Update PostApi.kt

--- app/src/main/java/app/web/postup/PostData/PostApi.kt
+++ app/src/main/java/app/web/postup/PostData/PostApi.kt
@@ -6,7 +6,7 @@ import io.reactivex.Observable
 import retrofit2.http.GET
 
 class PostApi{
-    interface GithubApiImpl {
+    interface getPostListApiImpl {
         @GET("/pupost")
         fun getPostList(): Observable<PostResponseModel>
     }
