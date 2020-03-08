99a3ef004da678b51bea289f9eea67082dfcff2d
rlarlvy153 <rlarlvy153@naver.com>
2020-03-08 20:22:46 +0900

    controll button align left

--- app/src/main/java/app/web/postup/Utils.kt
+++ app/src/main/java/app/web/postup/Utils.kt
@@ -1,12 +1,21 @@
 package app.web.postup
 
 import android.content.Context
+import android.util.DisplayMetrics
 import android.widget.Toast
+
 
 class Utils{
 
     companion object{
         fun showToast(context : Context, msg :String) = Toast.makeText(context,msg, Toast.LENGTH_SHORT).show()
+
+
+        fun pxToDp(context:Context,px: Int): Int {
+            val displayMetrics: DisplayMetrics = context.resources.displayMetrics
+            return Math.round(px / (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT))
+        }
     }
+
 
 }
