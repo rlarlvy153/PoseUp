667df9952d2edb10be79b498f404aa7a67d0db4a
rlarlvy153 <rlarlvy153@naver.com>
2020-08-04 23:07:32 +0900

    Update ViewModelModule.kt

--- app/src/main/java/kr/co/youngcha/postup/support/di/ViewModelModule.kt
+++ app/src/main/java/kr/co/youngcha/postup/support/di/ViewModelModule.kt
@@ -4,7 +4,6 @@ import kr.co.youngcha.postup.ui.MainViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
-
 val viewModelModule = module {
 
     viewModel { MainViewModel() }
